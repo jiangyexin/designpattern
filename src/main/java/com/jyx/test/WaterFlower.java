af50839f808ea63cfe4ee21c34e167ac48ae6f06
jiangyexin <JW1314>
2019-03-04 23:05:05 +0800

    水仙花数

---
+++ src/main/java/com/jyx/test/WaterFlower.java
@@ -0,0 +1,36 @@
+package com.jyx.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WaterFlower {
+    public static void main(String[] args) {
+        List result = new ArrayList();
+        for (int i = 1; i <= 10000; i++) {
+            int[] arr = intToArr(i);
+            if (powSum(arr) == i) {
+                result.add(i);
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+    public static int[] intToArr(int a) {
+        String numStr = String.valueOf(a);
+        int[] arr = new int[numStr.length()];
+        for (int i = 0; i < arr.length;i++) {
+            arr[i] = Integer.parseInt(String.valueOf(numStr.charAt(i)));
+        }
+        return arr;
+    }
+
+    public static int powSum(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result = result + arr[i] * arr[i] * arr[i];
+        }
+        return result;
+    }
+}
